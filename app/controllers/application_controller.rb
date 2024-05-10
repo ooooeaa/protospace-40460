6fd7712e34f0ec00e9072c878859bf81641056cc
ooooeaa <sodecoo0621@gmail.com>
2024-05-09 20:29:51 -0400

    5月9日

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :occupation, :potition])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :profile,:occupation, :position])
   end
 end
