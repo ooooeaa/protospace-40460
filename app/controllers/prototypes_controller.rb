9357d73e34635e60c807a39ee7f85ef6648f44e9
ooooeaa <sodecoo0621@gmail.com>
2024-05-12 15:38:47 -0400

    5月12日

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,18 @@
 class PrototypesController < ApplicationController
+
+  before_action :move_to_index, except: [:index, :show]
  def index
 end
 
+def new
+end
+
 def create
+end
+
+def move_to_index
+  unless user_signed_in?
+    redirect_to action: :index
+  end
 end
 end
